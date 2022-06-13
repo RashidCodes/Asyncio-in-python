14a1c2ad6fbed38827d3d29ea7e6577ce048879f
Rashid <rashid@Rashids-MacBook-Pro-2.local>
2022-06-13 17:10:43 +1000

    Count the number of exceptions that occurred

--- run_tasks_concurrently.py
+++ run_tasks_concurrently.py
@@ -2,7 +2,7 @@
 
 import asyncio
 
-async def factorial(name, number):
+async def factorial(name: str, number: int) -> int:
     f = 1
     for i in range(2, number + 1):
         print(f"Task {name}: Compute factorial({number}), currently i={i}...")
@@ -13,14 +13,19 @@ async def factorial(name, number):
 
 async def main():
     # Schedule three calls *concurrently*:
-    L = await asyncio.gather(
+    done, pending = await asyncio.wait([
         factorial("A", 2),
         factorial("E", "rashid"),
         factorial("B", 3),
         factorial("C", 4),
-        return_exceptions=False #default
-    )
-    print(L)
+    ])
+    print(f"These tasks were successfully executed: {done}")
+
+
+
+    # looking for tasks that completed with exceptions 
+    exceptions = len([task for task in done if 'exception' in str(task)])
+    print(f"Number of exceptions: {exceptions}")
 
 
 asyncio.run(main())
